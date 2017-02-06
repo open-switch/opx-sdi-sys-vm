40ddbede9939579e128e3f755791320efdb7b0d7
J.T. Conklin <J.T.Conklin@Dell.Com>
2017-02-05 18:35:27 -0800

    Add standard copyright boilerplate
    Signed-off-by: J.T. Conklin <J.T.Conklin@Dell.Com>

--- src/sdi_vm_fan.c
+++ src/sdi_vm_fan.c
@@ -1,9 +1,22 @@
 /*
- * sdi_vm_fan.c
- * Fan simulation functionality implements sdi-api headers
+ * Copyright (c) 2016 Dell Inc.
  *
- * Copyright (c) 2015, Dell Inc. All Rights Reserved
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
+ * THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
+ * LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS
+ * FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+ *
+ * See the Apache Version 2.0 License for specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * @file sdi_vm_fan.c
+ * @brief Fan simulation functionality implements sdi-api headers
  */
 
 #include "sdi_sys_vm.h"
